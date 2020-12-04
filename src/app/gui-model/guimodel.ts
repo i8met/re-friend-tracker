0c5aa4659cee2b2cf6623c60235a9cc72abff2f4
i8met <i.bakiji@hotmail.com>
2020-12-04 19:40:55 +0000

    Hallo1

--- src/app/gui-model/guimodel.ts
+++ src/app/gui-model/guimodel.ts
@@ -6,7 +6,7 @@ export class GuiModel {
 
     private _guiModel = {
         "application": {
-            "title": "Requirements Engineering Friend Tracker",
+            "title": "Ismet's Friend Tracker",
             "formList": [
                 {
                     "id": "FriendForm",
@@ -27,6 +27,13 @@ export class GuiModel {
                             "width": 1,
                             "required": true
                         },
+                            {
+                            "id": "nickname",
+                            "type": "text",
+                            "name": "Nickname",
+                            "width": 2,
+                            "required": true
+                            },
                         {
                             "id":   "location",
                             "type": "autocomplete",
@@ -60,7 +67,26 @@ export class GuiModel {
                         {
                             "type": "okButton",
                             "name": "Ok"
-                        }
+                        },
+
+                        {
+                            "id": "group",
+                            "type": "autocomplete",
+                            "name": "Group",
+                            "url": "/group",
+                            "form": "GroupForm",
+                            "width": 2
+                        },
+
+                        {
+                            "id": "activity",
+                            "type": "autocomplete",
+                            "name": "activity",
+                            "url": "/activity",
+                            "form": "ActivityForm",
+                            "width": 2
+                        },
+                        
                     ]
                 },
                 {
@@ -115,9 +141,73 @@ export class GuiModel {
                         {
                             "type": "okButton",
                             "name": "Ok"
-                        }
+                        },
+
+                          
                     ]
                 },
+                        {
+                            "id": "GroupForm",
+                            "title": "Group",
+                            "url": "/group",
+                            "formFieldList": [
+                        {
+                            "id": "name",
+                            "type": "text",
+                            "name": "GroupName",
+                            "width": 2,
+                            "required": true
+                        },
+                        {
+                            "type": "deleteButton",
+                            "name": "Delete"
+                        },
+                        {
+                            "type": "cancelButton",
+                            "name": "Cancel"
+                        },
+                        {
+                            "type": "okButton",
+                            "name": "Ok"
+                        }
+                        ]
+                        },
+
+                        {
+                            "id": "ActivityForm",
+                            "title": "Activity",
+                            "url": "/activity",
+                            "formFieldList": [
+                        {
+                            "id": "name",
+                            "type": "text",
+                            "name": "ActivityName",
+                            "width": 2,
+                            "required": true
+                        },
+                        {
+                            "type": "deleteButton",
+                            "name": "Delete"
+                        },
+                        {
+                            "type": "cancelButton",
+                            "name": "Cancel"
+                        },
+                        {
+                            "type": "okButton",
+                            "name": "Ok"
+                        },
+
+                        {
+                            "id":   "location",
+                            "type": "autocomplete",
+                            "name": "Location",
+                            "url": "/location",
+                            "form": "LocationForm",
+                            "width": 2
+                        },
+                        ]
+                        }
             ],
             "pageList": [
                 {
@@ -138,6 +228,24 @@ export class GuiModel {
                             "color": "yellow",
                             "page": "locationspage",
                         },
+
+                        {
+                            "type": "button",
+                            "name": "Groups",
+                            "icon": "fa-weixin",
+                            "color": "wisteria",
+                            "page": "groupspage",
+                        },
+
+                        {
+                            "type": "button",
+                            "name": "Activity",
+                            "icon": "fa-weixin",
+                            "color": "wisteria",
+                            "page": "activitypage",
+                        }
+
+                        
                     ]
                 },
                 {
@@ -162,9 +270,13 @@ export class GuiModel {
                             "search": true,
                             "url": "/friend",
                             "form": {
-                                "form": "FriendForm"
-                            }
+                                "form": "AddActivityForm"
+                            },
+                            
+
                         },
+
+                      
                     ]
                 },
                 {
@@ -195,6 +307,66 @@ export class GuiModel {
                         },
                     ]
                 },
+
+                        {
+                            "id": "groupspage",
+                             "elementList": [
+                        {
+                            "type": "backbutton",
+                        },
+                        {
+                            "type": "newButton",
+                            "name": "NewGroup",
+                            "icon": "fa-weixin",
+                            "color": "green",
+                            "form": {
+                            "form": "GroupForm"
+                        }
+                        },
+
+                        {
+                            "type": "list",
+                            "icon": "fa-weixin",
+                            "color": "wisteria",
+                            "search": true,
+                            "url": "/group",
+                            "form": {
+                            "form": "GroupForm"
+}
+},
+ ]
+},
+       {
+                            "id": "activitypage",
+                             "elementList": [
+                        {
+                            "type": "backbutton",
+                        },
+                        {
+                            "type": "newButton",
+                            "name": "NewGroup",
+                            "icon": "fa-weixin",
+                            "color": "green",
+                            "form": {
+                            "form": "ActivityForm"
+                        }
+                        },
+
+                        {
+                            "type": "list",
+                            "icon": "fa-weixin",
+                            "color": "wisteria",
+                            "search": true,
+                            "url": "/activity",
+                            "form": {
+                            "form": "ActivityForm"
+}
+},
+ ]
+}
+
+
+
             ]
         }
     };
